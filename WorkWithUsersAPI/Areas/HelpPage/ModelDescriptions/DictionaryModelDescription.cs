3942b162a9bd390f6fc14c59c4ece52a68c5c606
Sasha.Mykytyuk86 <Sasha.Mykytyuk86@gmail.com>
2019-01-18 18:02:09 +0200

    Add project files.

---
+++ WorkWithUsersAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WorkWithUsersAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WorkWithUsersAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WorkWithUsersAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
