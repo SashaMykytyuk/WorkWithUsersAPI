567b456b23777cef936008bb2dd14186d0241b7d
Sasha <Sasha.mykytyuk86@ukr.net>
2019-01-22 17:05:38 +0200

    open acsses for react

--- WorkWithUsersAPI/Controllers/AccountController.cs
+++ WorkWithUsersAPI/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.ModelBinding;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -19,6 +20,7 @@ using WorkWithUsersAPI.Results;
 
 namespace WorkWithUsersAPI.Controllers
 {
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     [Authorize]
     [RoutePrefix("api/Account")]
     public class AccountController : ApiController
